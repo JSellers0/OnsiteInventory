e97ba42aabdb849b610385132c0b681e70d50ed9
jsellers0 <JSellers0@Gmail.com>
2018-11-30 12:55:19 -0500

    Finished Tutorial

--- app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
+++ app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
@@ -3,13 +3,28 @@ package example.jsellers0.onsiteinventory
 //Version to practice Tutorial on.  Create a new branch to run through the tutorial so this stays at the beginning.
 //Tutorial located here: https://developer.android.com/training/basics/firstapp/
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "example.jsellers0.onsiteinventory.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**Called when the user taps the Send button */
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply{
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
