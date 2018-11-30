8e2d67d0e5b93f6329322becdb90665ae31be94c
jsellers0 <JSellers0@Gmail.com>
2018-11-30 11:23:34 -0500

    Initial Tutorial Commit.  Branch to complete Tutorial so others can start from the beginning.

--- app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
+++ app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
@@ -1,5 +1,8 @@
 package example.jsellers0.onsiteinventory
 
+//Version to practice Tutorial on.  Create a new branch to run through the tutorial so this stays at the beginning.
+//Tutorial located here: https://www.londonappdeveloper.com/how-to-clone-a-github-project-on-android-studio/
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
