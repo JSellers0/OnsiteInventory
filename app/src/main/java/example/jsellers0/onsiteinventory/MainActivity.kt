94c4ddcd60038e5cf94363e67da7f6a99974ccbf
jsellers0 <JSellers0@Gmail.com>
2018-11-30 17:24:01 -0500

    Lots of work. Moving from Start Inventory to Inventory crashes no matter which button you click.  In the future, probably shouldn't commit with bugs, but there's a lot of groundwork.

--- app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
+++ app/src/main/java/example/jsellers0/onsiteinventory/MainActivity.kt
@@ -1,12 +1,28 @@
 package example.jsellers0.onsiteinventory
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val SHIPMENT_TYPE = "example.jsellers0.onsiteinventory.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startInventory(view: View) {
+        val shipment = when (view.id) {
+            R.id.inbound_button -> {"inbound"}
+            R.id.outbound_button -> {"outbound"}
+            else -> {"ERROR"}
+        }
+        val begin = Intent(this, StartInventory::class.java).apply {
+            putExtra(SHIPMENT_TYPE, shipment)
+        }
+        startActivity(begin)
     }
 }
